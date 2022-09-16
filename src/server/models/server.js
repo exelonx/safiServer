22f43dd4c98b397809878e060777b82b223b0948
Kevin Cubas <kevin.otaku@hotmail.com>
2022-09-15 21:33:22 -0600

    Conexion con parametros de la db

--- src/server/models/server.js
+++ src/server/models/server.js
@@ -1,47 +1,60 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const colors = require('colors/safe');
 
+// Importación de la conexión y parametros de la DB
 const { db } = require('../database/db-conexion');
+const Parametro = require('../models/seguridad/parametro');
+const { clear } = require('console');
+
+// Routers de las APIs
+// const routerAuth = require('../routes/seguridad/auth.routes');
 
 class Server {
     constructor () {
-        //Configuración de express y WebSocket
+        // Configuración de express y WebSocket
         this.app    = express();
         this.http   = require('http').Server(this.app);
         this.io     = require('socket.io')(this.http, {
+            // Cors para los sockets
             cors: {
                 origin: '*'
             }
         });
 
-        //Puertos y rutas
-        this.port    = process.env.PORT;
+        // rutas
         this.apiPath = {
             auth:            '/api/auth',
             usuario:         '/api/usuario',
             rol:             '/api/rol',
             preguntaUsuario: '/api/pregunta-usuario'
         }
-
-        this.dbConnetion();
+        
+        this.conexionDB();
         this.middlewares();
         // this.routes();
         this.websocket();
     }
 
-    async dbConnetion () {
+    async conexionDB () {
         try {
             await db.authenticate();
-            console.log('Base de datos en linea');
+            const sistemaNombre = await Parametro.findOne({where: { PARAMETRO: 'SYS_NOMBRE' } });
+            console.log(colors.underline.cyan(sistemaNombre.VALOR))
+            console.log(colors.bold.green('Base de datos online'));
         } catch (error) {
             console.log(error)
             throw new Error(error);
         }
     }
 
+    async llamarParametros () {
+        return await Parametro.findAll();
+    }
+
     middlewares () {
-        //Cors
+        //Cors para express
         this.app.use(cors());
 
         //Lectura del body
@@ -49,14 +62,17 @@ class Server {
     }
 
     routes () {
-        //Seguridad
-        this.app.use(this.apiPath.usuario, routerUsuario)          //Usuarios
-        this.app.use(this.apiPath.rol, routerRol)                  //Roles
+        // Seguridad
+        this.app.use(this.apiPath.auth, routerAuth)         //Autenticación (Login)
+        // this.app.use(this.apiPath.usuario, routerUsuario)   //Usuarios
     }
 
-    listen () {
-        this.http.listen(this.port, () => {
-            console.log(`Corriendo en el puerto: ${this.port}`)
+    async listen () {
+        // Extraer el parametro del puerto
+        const puerto = await Parametro.findOne({where: { PARAMETRO: 'ADMIN_CPUERTO' } });
+        // Levantar servidor
+        this.http.listen(puerto.VALOR, () => {
+            console.log(`\nCorriendo en el puerto: ${colors.bold.red(puerto.VALOR)}`)
         })
     }
 
