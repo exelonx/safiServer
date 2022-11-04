92bf0e3500d0e3d49ed03a96659514f947a6b092
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-11-03 22:22:36 -0600

    API controlador de reporteria de rol

--- src/server/routes/reporteria/reporteria.routes.js
+++ src/server/routes/reporteria/reporteria.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { getReporteBitacora } = require('../../controllers/reporteria/bitacora.report.controllers');
+const { getReporteRol } = require('../../controllers/reporteria/rol.report.controller');
 
 const router = Router();
 
@@ -7,7 +8,7 @@ router.post('/bitacora', getReporteBitacora);
 
 router.post('/usuario', )
 
-router.post('/rol', )
+router.post('/rol', getReporteRol)
 
 router.post('/parametro', )
 
