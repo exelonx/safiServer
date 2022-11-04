6854ddbfe7fb92ebdd3a9aa53aa8f55c067f31b1
scarleth14 <abigailb643@gmail.com>
2022-11-03 22:33:11 -0600

    Reporte de parametros

--- src/server/routes/reporteria/reporteria.routes.js
+++ src/server/routes/reporteria/reporteria.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { getReporteBitacora } = require('../../controllers/reporteria/bitacora.report.controllers');
+const { getReporteParametro } = require('../../controllers/reporteria/parametro.report.controller');
 const { getReporteRol } = require('../../controllers/reporteria/rol.report.controller');
 
 const router = Router();
@@ -10,6 +11,6 @@ router.post('/usuario', )
 
 router.post('/rol', getReporteRol)
 
-router.post('/parametro', )
+router.post('/parametro', getReporteParametro )
 
 module.exports = router
