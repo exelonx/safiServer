af12b2d21481e4dba52073f354c1d1616c912d6a
Anthony Avila <anthony.avila.aa2@gmail.com>
2022-11-03 23:19:14 -0600

    API reporteria de usuario

--- src/server/routes/reporteria/reporteria.routes.js
+++ src/server/routes/reporteria/reporteria.routes.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const { getReporteBitacora } = require('../../controllers/reporteria/bitacora.report.controllers');
 const { getReporteParametro } = require('../../controllers/reporteria/parametro.report.controller');
 const { getReporteRol } = require('../../controllers/reporteria/rol.report.controller');
+const { getReporteUsuario } = require('../../controllers/reporteria/usuario.report.controller');
+
 
 const router = Router();
 
 router.post('/bitacora', getReporteBitacora);
 
-router.post('/usuario', )
+router.post('/usuario', getReporteUsuario);
 
 router.post('/rol', getReporteRol)
 
