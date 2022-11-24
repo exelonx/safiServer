8ca91225493f9c1c2c47053c34d9061ebf40a70d
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-11-23 23:16:45 -0600

    Pausa por errores en APIs de estado

--- src/server/routes/pedido/estado.routes.js
+++ src/server/routes/pedido/estado.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check, body } = require('express-validator');
-const { getInsumos, getInsumo, postInsumo, deleteInsumo, putInsumo } = require('../../controllers/inventario/insumo.controllers');
+const { getEstados, putEstado, getEstado } = require('../../controllers/pedido/estado.controllers');
 
 const { validarCampos, validarEspacio, validarDobleEspacio } = require('../../middlewares');
 const { validarEspaciosProveedor } = require('../../middlewares/validaciones-proveedores');
@@ -8,10 +8,10 @@ const { existeProveedor, noExisteProveedorPorId } = require('../../middlewares/v
 
 const router = Router();
 
-router.get('/', getInsumos);
+router.get('/', getEstados);
 
-router.get('/:id_insumo', getInsumo);
-
+router.get('/:id', getEstado);
+/* 
 router.post('/', [
     //Validaciones para el nombre del insumo
     check("nombre", "El nombre de insumo es obligatorio").not().isEmpty(), 
@@ -29,27 +29,27 @@ router.post('/', [
     //ID del usuario que lo creo
     check("creado_por", "El id del usuario que lo creo es obligatorio").not().isEmpty(), 
     validarCampos
-],postInsumo);
+],postInsumo);*/
 
-router.put('/:id_insumo', [
+router.put('/editar-estado/:id', [
     //Validaciones para el nombre del insumo
-    check("nombre", "El nombre de insumo es obligatorio").not().isEmpty(), 
-    check("nombre", "El nombre sólo permite letras").isAlpha("es-ES", {ignore: ' '}),
-    check("nombre", 'El nombre debe estar en mayúscula').isUppercase(),
-    check("nombre", "No se permite más de un espacio en blanco entre palabras").custom(validarDobleEspacio),
+    check("estado", "El nombre de insumo es obligatorio").not().isEmpty(), 
+    /* check("estado", "El nombre sólo permite letras").isAlpha("es-ES", {ignore: ' '}),
+    check("estado", 'El nombre debe estar en mayúscula').isUppercase(),
+    check("estado", "No se permite más de un espacio en blanco entre palabras").custom(validarDobleEspacio), */
     //Obliga a escribir la unidad de medida del insumo
-    check("id_unidad", "La unidad de medida es obligatoria").not().isEmpty(), 
+    /* check("id_unidad", "La unidad de medida es obligatoria").not().isEmpty(),  */
     //Validaciones de cantidad maxima 
-    check("cantidad_maxima", "La cantidad máxima es obligatoria").not().isEmpty(), 
-    check("cantidad_maxima", "La cantidad máxima debe ser un número").isNumeric(),
+    /* check("cantidad_maxima", "La cantidad máxima es obligatoria").not().isEmpty(), 
+    check("cantidad_maxima", "La cantidad máxima debe ser un número").isNumeric(), */
     //Validaciones de cantidad minima 
-    check("cantidad_minima", "La cantidad mínima es obligatoria").not().isEmpty(),
-    check("cantidad_minima", "La cantidad mínima debe ser un número").isNumeric(),
+    /* check("cantidad_minima", "La cantidad mínima es obligatoria").not().isEmpty(),
+    check("cantidad_minima", "La cantidad mínima debe ser un número").isNumeric(), */
     //ID del usuario que lo creo
     check("quienModifico", "El id del usuario que lo creo es obligatorio").not().isEmpty(), 
     validarCampos
-], putInsumo);
+], putEstado);
 
-router.delete('/:id_insumo', deleteInsumo)
+/* router.delete('/:id_insumo', deleteInsumo)  */
 
 module.exports = router;
