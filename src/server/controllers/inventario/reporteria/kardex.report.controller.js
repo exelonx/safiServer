7f96cd1b29e2d04e29e751b70bf2db1f6fafc10a
Anthony Avila <anthony.avila.aa2@gmail.com>
2022-11-28 02:27:56 -0600

    reporteria de movimientos

---
+++ src/server/controllers/inventario/reporteria/kardex.report.controller.js
@@ -0,0 +1,117 @@
+const { request, response } = require('express');
+const puppeteer = require('puppeteer');
+const { Op } = require('sequelize');
+const base64 = require('node-base64-image');
+
+// Importar librerias de fechas
+const dayjs = require('dayjs');
+const localizedFormat = require('dayjs/plugin/localizedFormat');
+
+const viewKardex = require('../../../models/inventario/sql-vista/view_kardex')
+const Parametro = require('../../../models/seguridad/parametro');
+
+const { compilarTemplate } = require('../../../helpers/compilarTemplate');
+
+const getReporteKardex = async (req = request, res = response) => {
+
+    let { buscar = "" } = req.body
+
+    try {
+
+        const buscador = await puppeteer.launch({headless: true});
+        const pagina = await buscador.newPage();
+
+        const kardex = await viewKardex.findAll({
+            where: {
+                [Op.or]: [{                    
+                    USUARIO: { [Op.like]: `%${buscar.toUpperCase() }%`}
+                }, {
+                    NOMBRE: { [Op.like]: `%${buscar.toUpperCase()}%`}
+                }, {
+                    UNIDAD_MEDIDA: { [Op.like]: `%${buscar.toUpperCase()}%`}
+                }, {
+                    CANTIDAD: { [Op.like]: `%${buscar.toUpperCase()}%`}
+                }, {
+                    TIPO_MOVIMIENTO: { [Op.like]: `%${buscar.toUpperCase()}%`}
+                }, {
+                    FECHA_Y_HORA: { [Op.like]: `%${buscar.toUpperCase()}%`}
+                }]
+            }
+        })
+
+        
+        const kardexMapped = kardex.map(( kardexs )=> {
+            return {
+                USUARIO: kardexs.USUARIO,
+                ID_INSUMO: kardexs.ID_INSUMO,
+                NOMBRE: kardexs.NOMBRE,
+                UNIDAD_MEDIDA: kardexs.UNIDAD_MEDIDA,
+                CANTIDAD: kardexs.CANTIDAD,
+                TIPO_MOVIMIENTO: kardexs.TIPO_MOVIMIENTO,
+                FECHA_Y_HORA: dayjs(kardexs.FECHA_Y_HORA).format('D MMM, YYYY , h:mm A')
+            }
+        })
+
+        const content = await compilarTemplate('kardex', {kardexs: kardexMapped})
+    
+        await pagina.setContent(content)
+        const options = {
+            string: true,
+            headers: {
+              "User-Agent": "my-app"
+            }
+          };
+
+        // Traer enlace del loco
+        const parametroLogo = await Parametro.findOne({
+            where: { PARAMETRO: 'LOGO'}
+        })
+
+        // Traer nombre de la empresa
+        const nombreEmpresa = await Parametro.findOne({
+            where: { PARAMETRO: 'NOMBRE_EMPRESA'}
+        })
+
+        // Convertir logo a base 64
+        const logo = await base64.encode(parametroLogo.VALOR, options);
+
+        await pagina.emulateMediaType("print")
+        const pdf = await pagina.pdf({
+            format: 'A4',
+            landscape: true,
+
+            margin: {
+                top: '100px',
+                bottom: '100px',
+            },
+            displayHeaderFooter: true,
+            printBackground: true,
+            headerTemplate: `
+            <div style="font-size:10px; margin: 0 auto; margin-left: 20px; margin-right: 20px;  width: 100%; display: flex; align-items: center; justify-content: space-between;" >  
+            <div style="color: #d12609; width: 22%;"><p>Fecha: <span class="date"></span></p></div>   
+            <div style="display: flex; width: 60%; margin: 0 auto; align-items: center; justify-content: center; flex-direction: column"><div style="font-weight: bold; font-size: 20px;">${nombreEmpresa.VALOR}</div> <div style="color: #d12609; font-size: 20px;">Reporte de movimientos de inventario</div></div>   
+            <div style=" display: flex; justify-content: end;  width: 20%;">
+            <img class="logo" alt="title" src="data:image/png;base64,${logo}" width="40"/>
+            </div></div>`,
+            footerTemplate: `<div style="font-size:10px; display: flex; justify-content: end;  width: 100%; margin-left: 20px; margin-right: 20px;">
+            <p>Página # <span class="pageNumber"></span> de <span class="totalPages"></span></p>
+            </div>`
+        })
+
+        await buscador.close()
+        console.log('descargar')
+
+        res.contentType("application/pdf");
+        res.send(pdf);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: error.message
+        })
+    }
+
+}
+
+module.exports = {
+    getReporteKardex
+}
--- src/server/routes/inventario/kardex.routes.js
+++ src/server/routes/inventario/kardex.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check, body } = require('express-validator');
 const { getKardex, validarIdInsumoKardex, getNombreInsumo } = require('../../controllers/inventario/kardex.controllers');
+const { getReporteKardex } = require('../../controllers/inventario/reporteria/kardex.report.controller');
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.get('/', getKardex);
 router.get('/validar/:id_insumo', validarIdInsumoKardex);
 
 router.get('/nombre/:id_insumo', getNombreInsumo);
+
+router.post('/reporteria/kardex', getReporteKardex);
 
 module.exports = router;
