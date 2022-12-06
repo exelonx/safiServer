0c11b2134af68583c3b70d6d85db16d8d218cf66
Kevin Cubas <kevin.otaku@hotmail.com>
2022-12-06 01:56:19 -0600

    front en enviroments y api de vista cliente

--- src/server/controllers/pedido/cocina.controllers.js
+++ src/server/controllers/pedido/cocina.controllers.js
@@ -48,6 +48,95 @@ const getDetallesCocina = async (req = request, res = response) => {
         // Contar resultados total
         const countDetalles = await ViewDetallePedido.count({
             where: {
+                [Op.not]: [
+                    {
+                        [Op.or]: [{
+                            ID_ESTADO: 3
+                        }, {
+                            ID_ESTADO: 4
+                        }, {
+                            ID_ESTADO: 5
+                        }, {
+                            ID_ESTADO: 6
+                        }]
+                    }
+                ],
+                [Op.or]: [{
+                    MESA: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }, {
+                    NOMBRE_PRODUCTO: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }, {
+                    ESTADO: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }]
+            }
+        });
+
+        res.json({ limite, countDetalles, detalles })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: error.message
+        })
+    }
+}
+
+const getDetallesVistaCliente = async (req = request, res = response) => {
+
+    let { limite, desde = 0, buscar = "", quienBusco = "" } = req.query
+
+    try {
+
+        // Definir el número de objetos a mostrar
+        if (!limite || limite === "") {
+            const { VALOR } = await Parametro.findOne({ where: { PARAMETRO: 'ADMIN_NUM_REGISTROS' } })
+            limite = VALOR;
+        }
+
+        if (desde === "") {
+            desde = 0;
+        }
+
+        const detalles = await ViewDetallePedido.findAll({
+            limit: parseInt(limite, 10),
+            offset: parseInt(desde, 10),
+            where: {
+                [Op.not]: [
+                    {
+                        [Op.or]: [ {
+                            ID_ESTADO: 4
+                        }, {
+                            ID_ESTADO: 5
+                        }, {
+                            ID_ESTADO: 6
+                        }]
+                    }
+                ],
+                [Op.or]: [{
+                    MESA: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }, {
+                    NOMBRE_PRODUCTO: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }, {
+                    ESTADO: { [Op.like]: `%${buscar.toUpperCase()}%` }
+                }]
+            }
+        });
+
+        // Contar resultados total
+        const countDetalles = await ViewDetallePedido.count({
+            where: {
+                [Op.not]: [
+                    {
+                        [Op.or]: [ {
+                            ID_ESTADO: 4
+                        }, {
+                            ID_ESTADO: 5
+                        }, {
+                            ID_ESTADO: 6
+                        }]
+                    }
+                ],
                 [Op.or]: [{
                     MESA: { [Op.like]: `%${buscar.toUpperCase()}%` }
                 }, {
@@ -70,5 +159,6 @@ const getDetallesCocina = async (req = request, res = response) => {
 }
 
 module.exports = {
-    getDetallesCocina
+    getDetallesCocina,
+    getDetallesVistaCliente
 }
--- src/server/routes/pedido/cocina.routes.js
+++ src/server/routes/pedido/cocina.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check, body } = require('express-validator');
-const { getDetallesCocina } = require('../../controllers/pedido/cocina.controllers');
+const { getDetallesCocina, getDetallesVistaCliente } = require('../../controllers/pedido/cocina.controllers');
 const { getReporteCocina } = require('../../controllers/pedido/reporteria/cocina.report.controller');
 
 const { validarCampos } = require('../../middlewares');
@@ -8,6 +8,8 @@ const { validarCampos } = require('../../middlewares');
 const router = Router();
 
 router.get('/', getDetallesCocina);
+
+router.get('/vista-clientes/', getDetallesVistaCliente);
 
 router.post('/reporteria/cocina', getReporteCocina);
 
